19cb12d18d34abf972d81780271242a3c946ff3b
Jacob Maine <jacob.maine@gmail.com>
2014-12-20 18:06:52 -0800

    Copy decisions previously stored in config file

--- lib/license_finder_upgrade/to_decisions.rb
+++ lib/license_finder_upgrade/to_decisions.rb
@@ -1,5 +1,36 @@
 module LicenseFinderUpgrade
-  module ToDecisions
+  class ToDecisions
+    def self.default_txn
+      {
+        who: "license_finder_upgrade",
+        why: "Upgrading from license_finder 0.8",
+        when: Time.now.getutc
+      }
+    end
+
+    def initialize(options)
+      @config = options.fetch(:config)
+      @txn = options.fetch(:txn) { default_txn }
+      @decisions = Decisions.new
+      prepare
+    end
+
+    attr_reader :decisions
+
+    private
+
+    def prepare
+      @decisions.name_project(@config.project_name, @txn)
+      @config.whitelist.each do |license|
+        @decisions.whitelist(license, @txn)
+      end
+      @config.ignore_dependencies.each do |dep|
+        @decisions.ignore(dep, @txn)
+      end
+      @config.ignore_groups.each do |group|
+        @decisions.ignore_group(group, @txn)
+      end
+    end
 
   end
 end
--- spec/lib/license_finder_upgrade/to_decisions_spec.rb
+++ spec/lib/license_finder_upgrade/to_decisions_spec.rb
@@ -2,10 +2,37 @@ require "spec_helper"
 
 module LicenseFinderUpgrade
   describe ToDecisions do
-    it "copies project name from yaml"
-    it "copies whitelist from yaml"
-    it "copies ignored dependencies from yaml"
-    it "copies ignored groups from yaml"
+    let(:txn) { {some: 'txn data'} }
+
+    def from_config(config)
+      described_class.new(
+        config: Configuration.new(config),
+        txn: txn
+      ).decisions.decisions
+    end
+
+    it "copies project name from yaml" do
+      expect(from_config("project_name" => "proj")).to include [:name_project, "proj", txn]
+    end
+
+    it "copies whitelist from yaml" do
+      decisions = from_config("whitelist" => ["lic1", "lic2"])
+      expect(decisions).to include [:whitelist, "lic1", txn]
+      expect(decisions).to include [:whitelist, "lic2", txn]
+    end
+
+    it "copies ignored dependencies from yaml" do
+      decisions = from_config("ignore_dependencies" => ["dep1", "dep2"])
+      expect(decisions).to include [:ignore, "dep1", txn]
+      expect(decisions).to include [:ignore, "dep2", txn]
+    end
+
+    it "copies ignored groups from yaml" do
+      decisions = from_config("ignore_groups" => ["grp1", "grp2"])
+      expect(decisions).to include [:ignore_group, "grp1", txn]
+      expect(decisions).to include [:ignore_group, "grp2", txn]
+    end
+
     it "copies manually created dependencies from db"
     it "copies manually licensed dependencies from db"
     it "copies manually approved licenses from db"
