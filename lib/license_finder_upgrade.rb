82803269faea8fcd671f03a8038b5bda5d0e8d87
Jacob Maine <jacob.maine@gmail.com>
2014-12-24 10:06:37 -0800

    Try to follow gem guidelines http://guides.rubygems.org/patterns/

--- lib/license_finder_upgrade.rb
+++ lib/license_finder_upgrade.rb
@@ -2,19 +2,19 @@ require "license_finder_upgrade/version"
 require 'yaml'
 
 module LicenseFinderUpgrade
-  ROOT_PATH = Pathname.new(__FILE__).dirname
-  autoload :Platform,      'license_finder_upgrade/platform'
-  autoload :Configuration, 'license_finder_upgrade/configuration'
-  autoload :Decisions,     'license_finder_upgrade/decisions'
-
-  autoload :ManualApproval, 'license_finder_upgrade/tables/manual_approval'
-  autoload :Dependency,     'license_finder_upgrade/tables/dependency'
-
-  autoload :ToDecisions, 'license_finder_upgrade/to_decisions'
-
   def self.config
     @config ||= Configuration.ensure_default
   end
 end
 
+
+require 'license_finder_upgrade/platform'
+require 'license_finder_upgrade/configuration'
+
+require 'license_finder_upgrade/decisions'
+
 require 'license_finder_upgrade/tables'
+require 'license_finder_upgrade/tables/manual_approval'
+require 'license_finder_upgrade/tables/dependency'
+
+require 'license_finder_upgrade/to_decisions'
